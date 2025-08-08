b0609fdc702490449cf17aeb5332a34afdda2969
nsLittle <notsolittle88@gmail.com>
2025-08-08 14:41:52 -0400

    Add post, Golden Fairy Rings

--- src/components/MainHeroIntro.tsx
+++ src/components/MainHeroIntro.tsx
@@ -6,12 +6,12 @@ export default function MainHeroIntro() {
         <br /> Guides & More
       </h1>
       <div className="mb-12 mt-8">
-        <p className="mt-4 text-3xl">
+        <p className="mt-4 text-2xl">
           Come discover the art of simple intentions, the joys of wild crafting,
           slowing down for seasonal coooking, and opportunistic foraging from
           Vermont.
         </p>
-        <p className="mt-4 text-3xl">
+        <p className="mt-4 text-2xl">
           This is where the warmth of woodsy treasures meet homemade goodness.
           It's about life's simple pleasures{" "}
           <span>savored with intentions.</span>
