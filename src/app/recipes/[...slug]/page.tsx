470dc353c21130452f28989afed30bbe1e6e637c
nsLittle <notsolittle88@gmail.com>
2025-09-09 10:24:25 -0400

    Make even more deployment fixes

--- src/app/recipes/[...slug]/page.tsx
+++ src/app/recipes/[...slug]/page.tsx
@@ -15,7 +15,7 @@ export async function generateStaticParams() {
 export default async function RecipePage({
   params,
 }: {
-  params: { slug: string[] };
+  params: Promise<{ slug: string[] }>;
 }) {
   const { slug } = await params;
   const slugParts = slug;
