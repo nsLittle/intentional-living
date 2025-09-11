bae2f61ee61aff90c746c7ca2f8847ee0f20f55a
nsLittle <notsolittle88@gmail.com>
2025-09-11 10:30:24 -0400

    Fix viewport error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#ffffff" },
     { media: "(prefers-color-scheme: dark)", color: "#0b0b0b" },
