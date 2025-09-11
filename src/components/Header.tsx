0d1be8b9f2fc40f699e991fea308fb8c5bddb37a
nsLittle <notsolittle88@gmail.com>
2025-09-11 11:32:16 -0400

    Fix width on header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,14 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="relative w-full h-58">
+    <header className="relative w-full">
       <Image
         src="/images/header-banner/log-garden.jpeg"
         alt="Intentional Living Banner"
         fill
         className="object-cover"
         priority
-        fetchPriority="high"
         sizes="100vw"
-        quality={70}
       />
     </header>
   );
