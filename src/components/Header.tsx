16c2c985c5399c49ad383b7fb5b65663d080ec9a
nsLittle <notsolittle88@gmail.com>
2025-09-15 14:49:10 -0400

    Fix Header height across all pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="relative w-full h-[220px] sm:h-[280px] md:h-[340px] lg:h-[420px]">
+    <header className="relative w-full h-[140px] sm:h-[180px] md:h-[220px] lg:h-[260px]">
       <Image
         src="/images/header-banner/log-garden.jpeg"
         alt="Intentional Living Banner"
